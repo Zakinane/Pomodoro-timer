e8afecb5c20d9103b4e6a3d7ea6f5ed5b735691d
Zak <djabazaki@gmail.com>
2025-05-07 19:12:40 +0100

    Timer V1.1.0

--- timer.js
+++ timer.js
@@ -4,73 +4,112 @@ let timer = document.querySelector(".timer");
 let progressBar = document.getElementById("progress-bar");
 let audio = new Audio("./audio/Reveil electronique.mp3");
 
-
 let rawInput = parseInt(prompt("Durée en secondes (max 5999) :"));
-let setTime = Math.min(rawInput, 5999);
-let time = setTime * 1000;
-progressBar.max = time;
-progressBar.value = 0;
+let duration = Math.min(rawInput, 5999);
+let timeStart; // When the start is pressed
+let timePaused = 0; // timePaused = timeRestart - timePaus
+let timeLeft; // Date.now - timeStart 
+
+progressBar.max = duration;
+progressBar.style.width = "0%";
 
 let isStarted = false;
 let intervalID = null;
 
-function calcDisplayedTime() {
-  minutes = Math.floor(time / 60000);
-  secondes = Math.floor((time % 60000) / 1000);
+function updateDisplay(t) {
+  minutes = Math.floor(t / 60);
+  secondes = t % 60;
+
+  timer.textContent =
+    String(minutes).padStart(2, "0") + ":" + String(secondes).padStart(2, "0"); // timer
+  document.title = "Timer : " + timer.textContent; // page title
+
 }
 
-function updateTimerText() {
-  calcDisplayedTime();
-  timer.textContent =
-    String(minutes).padStart(2, "0") + ":" + String(secondes).padStart(2, "0");
+function tick() {
+  if (!timeStart) return;
+
+  const now = Date.now();
+  const elapsed = Math.floor((now - timeStart) / 1000); // secondes passed
+  const timeLeft = Math.max(duration - elapsed, 0); // secondes left
+  progressBar.style.width = `${(elapsed / duration) * 100}%`;
+
+  updateDisplay(timeLeft);
+
+  if (timeLeft <= 0) {
+    clearInterval(intervalID);
+    intervalID = null;
+    isStarted = false;
+    stopStart.textContent = "Reset";
+    skip.style.display = "none";
+    audio.play();
+  }
 }
+
 
 function startTimer() {
-  if (intervalID !== null) return;
-  intervalID = setInterval(() => {
-    if (time > 0) {
-      updateTimerText();
-      progressBar.value = setTime * 1000 - time;
-      time -= 100;
-    } else {
-      stopStart.textContent = "Reset";
-      skip.style.display = "none";
-      audio.play();
-      clearInterval(intervalID);
-      intervalID = null;
-      isStarted = false;
-    }
-  }, 100);
+  // restart after pause
+  if (!timeStart) {
+    timeStart = Date.now();
+  } else {
+    timeStart += Date.now() - timePaused;
+  }
+  intervalID = setInterval(tick, 500);
+  updateDisplay(duration);
+  tick(); // no 1s delay
 }
+
 
 function pauseTimer() {
   clearInterval(intervalID);
   intervalID = null;
+  timePaused = Date.now();
 }
 
-updateTimerText();
+function resetTimer() {
+  rawInput = parseInt(prompt("Durée en secondes (max 5999) :"));
+  duration = Math.min(rawInput, 5999);
+  updateDisplay(duration);
+
+  clearInterval(intervalID);
+  intervalID = null;
+  isStarted = false;
+  timeStart = null;
+  timePaused = 0;
+  progressBar.style.width = "0%";
+  stopStart.textContent = "Start";
+  skip.style.display = "flex";
+  progressBar.style.width = "0%";
+  audio.pause();
+  audio.currentTime = 0; // restart beep beep from start
+}
+
+updateDisplay(duration);
 
 stopStart.addEventListener("click", () => {
-  if (time === 0) {
-    time = setTime * 1000;
-    progressBar.value = 0;
-    updateTimerText();
-    stopStart.textContent = "Start";
-    audio.pause();
+  // if finished click reset
+  if (!isStarted && progressBar.style.width === "100%") {
+    resetTimer();
     return;
   }
-  isStarted = !isStarted;
-  if (isStarted) {
-    skip.style.display = "flex";
-    stopStart.textContent = "Pause";
 
+  isStarted = !isStarted;
+
+  if (isStarted) {
+    stopStart.textContent = "Pause";
+    skip.style.display = "flex";
     startTimer();
-  } else if (time !== 0) {
+  } else {
     stopStart.textContent = "Restart";
     pauseTimer();
-  } else {
-    updateTimerText()
-    time = setTime;
   }
 });
- 
+
+skip.addEventListener("click", () => {
+  clearInterval(intervalID);
+  updateDisplay(0);
+  stopStart.textContent = "Reset";
+  skip.style.display = "none";
+  progressBar.style.width = "100%";
+  isStarted = false;
+});
